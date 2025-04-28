b41eeeaae89efff8c27b39c9f99f233cd824e5cb
Athira-vk <vkathiravk@gmail.com>
2025-04-28 13:52:27 +0530

    Add .gitignore to exclude unnecessary files

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-  await mongoose
-    .connect(
-      "mongodb+srv://athiravk:12345@cluster0.q62skss.mongodb.net/food-del"
-    )
-    .then(() => console.log("DB Connected"));
-};

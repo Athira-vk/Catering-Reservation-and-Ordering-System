734789ff6c3d25ff8abf54571367abc8551518a8
Athira-vk <vkathiravk@gmail.com>
2025-04-30 21:14:20 +0530

    Update admin

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 import { Routes, Route } from "react-router-dom";
 import Add from "./pages/Add/Add";
-import List from "./pages/List/LIst";
+import List from "./pages/List/List";
 import Orders from "./pages/Orders/Orders";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
+import Home from "./components/Home/Home";
 
 const App = () => {
   const url = "http://localhost:4000";
+  const [showLogin, setShowLogin] = useState(false);
+  const [token, setToken] = useState("");
 
   return (
     <div>
       <ToastContainer />
-      <Navbar />
+      {showLogin ? (
+        <LoginPopup url={url} setShowLogin={setShowLogin} setToken={setToken} />
+      ) : (
+        <></>
+      )}
+      <Navbar token={token} setToken={setToken} setShowLogin={setShowLogin} />
       <hr />
       <div className="app-content">
-        <Sidebar />
-        <Routes>
-          <Route path="/add" element={<Add url={url} />} />
-          <Route path="/list" element={<List url={url} />} />
-          <Route path="/orders" element={<Orders url={url} />} />
-        </Routes>
+        {!token ? (
+          <Home />
+        ) : (
+          <>
+            <Sidebar />
+            <Routes>
+              <Route path="/add" element={<Add url={url} />} />
+              <Route path="/list" element={<List url={url} />} />
+              <Route path="/orders" element={<Orders url={url} />} />
+            </Routes>
+          </>
+        )}
       </div>
     </div>
   );
--- admin/src/components/Navbar/Navbar.jsx
+++ admin/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets";
+import { useNavigate } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin, token, setToken }) => {
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    navigate("/");
+  };
+
   return (
     <div className="navbar">
       <img className="logo" src={assets.logo} alt="" />
-      <img className="profile" src={assets.profile_image} alt="" />
+      <div className="navbar-right">
+        {!token ? (
+          <button onClick={() => setShowLogin(true)}>sign in</button>
+        ) : (
+          <div className="navbar-profile">
+            <img className="profile" src={assets.profile_image} alt="" />
+            <button className="logout" onClick={logout}>
+              Logout
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };

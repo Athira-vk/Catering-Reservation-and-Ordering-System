a33081b42289e25a05cc18300fea044c9360a12c
Athira-vk <vkathiravk@gmail.com>
2025-05-11 15:46:14 +0530

    second commit

--- admin/src/components/Home/Home.jsx
+++ admin/src/components/Home/Home.jsx
@@ -2,7 +2,15 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <div className="background-photo">
+        <div className="photo">
+          <img src="/14589.jpg" alt="Background" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
